d5f388b099cd2f95e200ccfa2af2b183ae3f23a2
AXL <116296322+AXLShorts@users.noreply.github.com>
2024-08-12 17:40:12 +0500

    1

--- w4-assignment/server/dist/src/controllers/authController.js
+++ w4-assignment/server/dist/src/controllers/authController.js
@@ -61,7 +61,7 @@ const signin = async (req, res) => {
             sameSite: "none",
             maxAge: 60 * 60 * 24 * 7,
             expires: new Date(Date.now() + 60 * 60 * 24 * 7 * 1000),
-            domain: "http://127.0.0.1:3000",
+            domain: "127.0.0.1",
             path: "/",
         }));
         res.status(200).json({
