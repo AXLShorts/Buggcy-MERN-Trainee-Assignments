3ebdd053c1d8133de62c7a25f7a4dfc933c6eb75
AXL <116296322+AXLShorts@users.noreply.github.com>
2024-08-16 13:20:55 +0500

    1

--- w4-assignment/server/src/middleware/rateLimiter.ts
+++
@@ -1,7 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-export const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 100000, // 15 minutes
-  max: 10000, // limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again later",
-});
